25a37cc63dc68c5a19ac6e170d46374834c573f4
ijapesigan <ijapesigan@gmail.com>
2023-07-08 10:00:26 +0000

    Automated build.

--- .devcontainer/tools/setup.sh
+++ .devcontainer/tools/setup.sh
@@ -30,13 +30,3 @@ pip3 install -U radian
 install2.r --error --skipinstalled -n -1 \
   tidyverse \
   dplyr
-
-# tinytex full
-R -e "tinytex::install_tinytex( \
-    bundle = 'TinyTeX-2',       \
-    force = TRUE,               \
-    dir =  '/opt/TinyTeX'       \
-)"
-
-# rProject
-Rscript -e "remotes::install_github('ijapesigan/rProject')"
