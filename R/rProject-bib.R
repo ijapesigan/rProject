82765f283970dfd5a7d4f1633ca18ad8ca4edea8
ijapesigan <ijapesigan@users.noreply.github.com>
2023-08-14 14:38:12 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/rProject.pdf
    A	.setup/build/rProject_0.0.5.tar.gz
    A	.setup/data-analysis/data-analysis-mle-exponential.txt
    A	.setup/data-analysis/data-analysis-mle-normal.txt
    A	.setup/latex/bib/lib-1930-1939-article
    A	.setup/latex/bib/lib-1940-1949-article
    A	.setup/latex/bib/lib-1950-1959-article
    A	.setup/latex/bib/lib-1960-1969-article
    A	.setup/latex/bib/lib-1970-1979-article
    A	.setup/latex/bib/lib-1980-1989-article
    A	.setup/latex/bib/lib-1980-1989-book
    A	.setup/latex/bib/lib-1990-1999-article
    A	.setup/latex/bib/lib-1990-1999-book
    A	.setup/latex/bib/lib-2000-2009-article
    A	.setup/latex/bib/lib-2000-2009-book
    A	.setup/latex/bib/lib-2010-2019-article
    A	.setup/latex/bib/lib-2010-2019-book
    A	.setup/latex/bib/lib-2010-2019-manual
    A	.setup/latex/bib/lib-2020-2029-article
    A	.setup/latex/bib/lib-2020-2029-book
    A	.setup/latex/bib/lib-2020-2029-manual
    A	.setup/lint/.lintr
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE
    A	LICENSE.md
    A	NAMESPACE
    M	R/rProject-bib.R
    M	R/rProject-latex-compile.R
    A	README.md
    A	data/z.rda
    A	man/Bib.Rd
    A	man/Binary.Rd
    A	man/Build.Rd
    A	man/BuildIgnore.Rd
    A	man/CFF.Rd
    A	man/Clean.Rd
    A	man/CleanPkg.Rd
    A	man/CleanProj.Rd
    A	man/CleanTinyTex.Rd
    A	man/ConfigFiles.Rd
    A	man/Coverage.Rd
    A	man/DataAnalysis.Rd
    A	man/DataProcess.Rd
    A	man/Dependencies.Rd
    A	man/Detritus.Rd
    A	man/Environment.Rd
    A	man/EnvironmentProject.Rd
    A	man/EnvironmentTokens.Rd
    A	man/InstallLocal.Rd
    A	man/LatexCompile.Rd
    A	man/LatexKnit.Rd
    A	man/LatexMake.Rd
    A	man/LibPaths.Rd
    A	man/License.Rd
    A	man/Lint.Rd
    A	man/Manual.Rd
    A	man/PkgDevel.Rd
    A	man/PkgDevelGitHub.Rd
    A	man/PkgProject.Rd
    A	man/PkgProjectGitHub.Rd
    A	man/PkgProjectVersion.Rd
    A	man/PkgRepo.Rd
    A	man/Profile.Rd
    A	man/Project.Rd
    A	man/Quarto.Rd
    A	man/ReadMe.Rd
    A	man/Site.Rd
    A	man/Style.Rd
    A	man/TinyTex.Rd
    A	man/VignettesPrecompile.Rd
    A	man/z.Rd
    M	project.Rproj
    A	vignettes/lib-tags.Rmd
    A	vignettes/vignettes.bib

--- R/rProject-bib.R
+++ R/rProject-bib.R
@@ -20,23 +20,23 @@ Bib <- function(path) {
     )
   )
   libs <- c(
-	"lib-2020-2029-book",
-	"lib-2020-2029-manual",
-	"lib-2020-2029-article",
-	"lib-2010-2019-manual",
-	"lib-2010-2019-book",
-	"lib-2010-2019-article",
-	"lib-2000-2009-book",
-	"lib-2000-2009-article",
-	"lib-1990-1999-article",
-	"lib-1990-1999-book",
-	"lib-1980-1989-article",
-	"lib-1980-1989-book",
-	"lib-1970-1979-article",
-	"lib-1960-1969-article",
-	"lib-1950-1959-article",
-	"lib-1940-1949-article",
-	"lib-1930-1939-article"
+    "lib-2020-2029-book",
+    "lib-2020-2029-manual",
+    "lib-2020-2029-article",
+    "lib-2010-2019-manual",
+    "lib-2010-2019-book",
+    "lib-2010-2019-article",
+    "lib-2000-2009-book",
+    "lib-2000-2009-article",
+    "lib-1990-1999-article",
+    "lib-1990-1999-book",
+    "lib-1980-1989-article",
+    "lib-1980-1989-book",
+    "lib-1970-1979-article",
+    "lib-1960-1969-article",
+    "lib-1950-1959-article",
+    "lib-1940-1949-article",
+    "lib-1930-1939-article"
   )
   clean <- function(libs, path) {
     lapply(
