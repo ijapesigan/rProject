d8a02fb259cc1685bf98e1bbf164c6e78313334f
ijapesigan <ijapesigan@gmail.com>
2023-08-14 14:56:04 +0000

    Automated build.

--- R/rProject-bib.R
+++ R/rProject-bib.R
@@ -5,38 +5,38 @@
 #' @inheritParams LibPaths
 #' @export
 Bib <- function(path) {
+  latex_bib_dir <- file.path(
+    path,
+    ".setup",
+    "latex",
+    "bib"
+  )
+  if (dir.exists(latex_bib_dir)) {
   # lib
   wd <- getwd()
   on.exit(
     expr = setwd(wd),
     add = TRUE
   )
-  setwd(
-    file.path(
-      path,
-      ".setup",
-      "latex",
-      "bib"
-    )
-  )
+  setwd(latex_bib_dir)
   libs <- c(
-    "lib-2020-2029-book",
-    "lib-2020-2029-manual",
-    "lib-2020-2029-article",
-    "lib-2010-2019-manual",
-    "lib-2010-2019-book",
-    "lib-2010-2019-article",
-    "lib-2000-2009-book",
-    "lib-2000-2009-article",
-    "lib-1990-1999-article",
-    "lib-1990-1999-book",
-    "lib-1980-1989-article",
-    "lib-1980-1989-book",
-    "lib-1970-1979-article",
-    "lib-1960-1969-article",
-    "lib-1950-1959-article",
-    "lib-1940-1949-article",
-    "lib-1930-1939-article"
+	"lib-2020-2029-book",
+	"lib-2020-2029-manual",
+	"lib-2020-2029-article",
+	"lib-2010-2019-manual",
+	"lib-2010-2019-book",
+	"lib-2010-2019-article",
+	"lib-2000-2009-book",
+	"lib-2000-2009-article",
+	"lib-1990-1999-article",
+	"lib-1990-1999-book",
+	"lib-1980-1989-article",
+	"lib-1980-1989-book",
+	"lib-1970-1979-article",
+	"lib-1960-1969-article",
+	"lib-1950-1959-article",
+	"lib-1940-1949-article",
+	"lib-1930-1939-article"
   )
   clean <- function(libs, path) {
     lapply(
@@ -49,12 +49,14 @@ Bib <- function(path) {
             "latex",
             "bib",
             lib
-          )
+          ),
+          recursive = TRUE
         )
       },
       path = path
     )
   }
+  clean(libs = libs, path = path)
   on.exit(
     expr = clean(libs = libs, path = path),
     add = TRUE
@@ -67,6 +69,7 @@ Bib <- function(path) {
         ".git"
       )
     )
+  }
   }
   # bib
   unlink(
@@ -106,8 +109,10 @@ Bib <- function(path) {
     bib_src <- list.files(
       path = latex_bib_dir,
       pattern = ".*\\.bib$",
+      all.files = TRUE,
       full.names = TRUE,
-      recursive = TRUE
+      recursive = TRUE,
+      include.dirs = TRUE
     )
     length_bib_src <- length(bib_src)
     if (length_bib_src > 0) {
