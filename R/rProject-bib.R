519a00c439d5f7e7bc63cb836a2007d388783325
jeksterslab <learn.jeksterslab@gmail.com>
2023-08-14 14:31:45 +0000

    Automated build.

--- R/rProject-bib.R
+++ R/rProject-bib.R
@@ -20,26 +20,43 @@ Bib <- function(path) {
     )
   )
   libs <- c(
-    "lib-2020-2029-book",
-    "lib-2020-2029-manual",
-    "lib-2020-2029-article",
-    "lib-2010-2019-manual",
-    "lib-2010-2019-book",
-    "lib-2010-2019-article",
-    "lib-2000-2009-book",
-    "lib-2000-2009-article",
-    "lib-1990-1999-article",
-    "lib-1990-1999-book",
-    "lib-1980-1989-article",
-    "lib-1980-1989-book",
-    "lib-1970-1979-article",
-    "lib-1960-1969-article",
-    "lib-1950-1959-article",
-    "lib-1940-1949-article",
-    "lib-1930-1939-article"
+	"lib-2020-2029-book",
+	"lib-2020-2029-manual",
+	"lib-2020-2029-article",
+	"lib-2010-2019-manual",
+	"lib-2010-2019-book",
+	"lib-2010-2019-article",
+	"lib-2000-2009-book",
+	"lib-2000-2009-article",
+	"lib-1990-1999-article",
+	"lib-1990-1999-book",
+	"lib-1980-1989-article",
+	"lib-1980-1989-book",
+	"lib-1970-1979-article",
+	"lib-1960-1969-article",
+	"lib-1950-1959-article",
+	"lib-1940-1949-article",
+	"lib-1930-1939-article"
   )
+  clean <- function(libs, path) {
+    lapply(
+      X = libs,
+      FUN = function(lib, path) {
+        unlink(
+          file.path(
+            path,
+            ".setup",
+            "latex",
+            "bib",
+            lib
+          )
+        )
+      },
+      path = path
+    )
+  }
   on.exit(
-    expr = unlink(libs),
+    expr = clean(libs = libs, path = path),
     add = TRUE
   )
   for (i in seq_along(libs)) {
