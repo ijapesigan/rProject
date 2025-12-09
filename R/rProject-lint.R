76592f7a99ab0ad8b84e5a22d5c52083fc789246
ijapesigan <ijapesigan@gmail.com>
2025-12-09 00:03:09 +0000

    Automated build.

--- R/rProject-lint.R
+++ R/rProject-lint.R
@@ -16,22 +16,37 @@ Lint <- function(path) {
       quiet = TRUE
     )
   }
-  lintr::lint_dir(
-    path = path,
-    linters = lintr::linters_with_defaults(
-      lintr::object_name_linter(
-        styles = c(
-          "CamelCase",
-          "snake_case",
-          "symbols"
-        )
+  linters <- lintr::linters_with_defaults(
+    lintr::object_name_linter(
+      styles = c(
+        "CamelCase",
+        "snake_case",
+        "symbols"
       )
-    ),
-    exclusions = list(
-      "renv",
-      "packrat",
-      ".library",
-      "R/RcppExports.R"
     )
   )
+  exclusions <- list(
+    "renv",
+    "packrat",
+    ".library",
+    file.path("R", "RcppExports.R")
+  )
+  keep <- vapply(
+    X = exclusions,
+    FUN = function(x) file.exists(x),
+    FUN.VALUE = logical(1)
+  )
+  exclusions <- exclusions[keep]
+  if (length(exclusions) == 0L) {
+    lintr::lint_dir(
+      path = path,
+      linters = linters
+    )
+  } else {
+    lintr::lint_dir(
+      path = path,
+      linters = linters,
+      exclusions = exclusions
+    )
+  }
 }
